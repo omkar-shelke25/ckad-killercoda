c3335a5b37c8c1c7fb2023293172bf2f5be9ef1a
Omkar Shelke <85251955+omkar-shelke25@users.noreply.github.com>
2026-01-27 17:28:38 +0530

    Update setup.sh

--- qos-scenario/setup.sh
+++ qos-scenario/setup.sh
@@ -6,6 +6,6 @@ echo "📦 Setting up environment for CKAD Practice..."
 # Create mars namespace
 kubectl create namespace mars --dry-run=client -o yaml | kubectl apply -f -
 
-mkdir
+mkdir /opt/mars/
 
 echo "✅ mars namespace is ready for the scenario!"
